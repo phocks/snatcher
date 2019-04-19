ed5008e6e5fc87611fa4150bcf73de48ab713144
Joshua Byrd <phocks@gmail.com>
2019-04-19 19:52:43 +1000

    parcel does single js files

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
 import json from "rollup-plugin-json";
 import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
+// import babel from "rollup-plugin-babel";
 
 export default {
   input: "src/main.js",
   output: {
     file: "background.js",
-    format: "cjs"
+    format: "iife"
   },
   plugins: [
     resolve(),
-    babel({
-      exclude: "node_modules/**" // only transpile our source code
-    }),
+    // babel({
+    //   exclude: "node_modules/**" // only transpile our source code
+    // }),
     json()
   ]
 };
--- src/main.js
+++
@@ -1,8 +0,0 @@
-// src/main.js
-import { version } from '../package.json';
-
-const hello = async () => {
-  console.log('version ' + version);
-}
-
-hello();
